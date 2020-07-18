6c1c9b7fb22e67798f6751807669fcf7fcc81c95
Maalej1995 <maalejh95@gmail.com>
2020-07-18 16:02:48 +0100

    React CP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Form from './Components/Form';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const App = () => {
   return (
